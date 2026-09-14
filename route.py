8afefd5dbfc71f90bc5180f1081f0a9a872efc5d
Arthur Silva <silvaarthur31807@gmail.com>
2026-09-14 09:32:20 -0300

    quatro paginas em branco!

--- route.py
+++ route.py
@@ -5,3 +5,15 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/3')
+def page3():
+    return render_template('page3.html')
+
+@app.route('/4')
+def page4():
+    return render_template('page4.html')

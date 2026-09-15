2b8d675c3250787da3d104fccc2b4eab2f4c684e
Allana141008 <167999535+Allana141008@users.noreply.github.com>
2026-09-15 19:45:37 -0300

    Fix order of gravidade and descricao columns

--- models.py
+++ models.py
@@ -6,11 +6,11 @@ class Ocorrencia(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ocorrencia = db.Column(db.String(100), nullable=False)
+    gravidade = db.Column(db.String(20), nullable=False)
     local = db.Column(db.String(100), nullable=False)
     data = db.Column(db.Date, nullable=False)
-    descricao = db.Column(db.Text, nullable=False)
     hora = db.Column(db.Time, nullable=False)
-    gravidade = db.Column(db.String(20), nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f'<Ocorrencia {self.ocorrencia}>'

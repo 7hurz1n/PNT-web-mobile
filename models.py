77efed831f2907af7252f7a77b7655ae2296eac8
Allana141008 <167999535+Allana141008@users.noreply.github.com>
2026-09-15 19:27:25 -0300

    Refactor Ocorrencia model to use Date and Text types

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from db import db
 
+
 class Ocorrencia(db.Model):
     __tablename__ = 'ocorrencias'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ocorrencia = db.Column(db.String(100), nullable=False)
-    detalhes = db.Column(db.String(200), nullable=False)
     local = db.Column(db.String(100), nullable=False)
-    data = db.Column(db.String(10), nullable=False)
-    hora = db.Column(db.String(5), nullable=False)
+    data = db.Column(db.Date, nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f'<Ocorrencia {self.ocorrencia}>'

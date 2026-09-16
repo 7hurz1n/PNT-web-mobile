49a7453959d278ecfeeb423af2694de2026101a5
Arthur Silva <silvaarthur31807@gmail.com>
2026-09-15 21:51:07 -0300

    Add gravidade field to Ocorrencia handling

--- route.py
+++ route.py
@@ -17,15 +17,16 @@ def register():
     elif request.method == 'POST':
         ocorrencia = request.form['ocorrenciaForm']
         detalhes = request.form['detalhesForm']
+        gravidade = request.form['gravidadeForm']
         local = request.form['localForm']
         data = request.form['dataForm']
         hora = request.form['horaForm']
 
-        nova_ocorrencia = Ocorrencia(ocorrencia=ocorrencia, detalhes=detalhes, local=local, data=data, hora=hora)
+        nova_ocorrencia = Ocorrencia(ocorrencia=ocorrencia, detalhes=detalhes, gravidade=gravidade, local=local, data=data, hora=hora)
         db.session.add(nova_ocorrencia)
         db.session.commit()
 
-        return render_template('confirmacao.html', ocorrencia=ocorrencia, detalhes=detalhes, local=local, data=data, hora=hora)
+        return render_template('confirmacao.html', ocorrencia=ocorrencia, detalhes=detalhes, gravidade=gravidade, local=local, data=data, hora=hora)
 
 @routes.route('/historico')
 def historico():
@@ -40,17 +41,18 @@ def editar(id):
     elif request.method == 'POST':
         ocorrencia.ocorrencia = request.form['ocorrenciaForm']
         ocorrencia.detalhes = request.form['detalhesForm']
+        ocorrencia.gravidade = request.form['gravidadeForm']
         ocorrencia.local = request.form['localForm']
         ocorrencia.data = request.form['dataForm']
         ocorrencia.hora = request.form['horaForm']
 
         db.session.commit()
 
-        return render_template('confirmacao.html', ocorrencia=ocorrencia.ocorrencia, detalhes=ocorrencia.detalhes, local=ocorrencia.local, data=ocorrencia.data, hora=ocorrencia.hora)
+        return render_template('confirmacao.html', ocorrencia=ocorrencia.ocorrencia, detalhes=ocorrencia.detalhes, local=ocorrencia.local, data=ocorrencia.data, hora=ocorrencia.hora, gravidade=ocorrencia.gravidade)
 
 @routes.route('/deletar/<int:id>', methods=['GET', 'POST'])
 def deletar(id):
     ocorrencia = Ocorrencia.query.get(id)
     db.session.delete(ocorrencia)
     db.session.commit()
-    return render_template('deletado.html', ocorrencia=ocorrencia.ocorrencia, detalhes=ocorrencia.detalhes, local=ocorrencia.local, data=ocorrencia.data, hora=ocorrencia.hora)
+    return render_template('deletado.html', ocorrencia=ocorrencia.ocorrencia, detalhes=ocorrencia.detalhes, local=ocorrencia.local, data=ocorrencia.data, hora=ocorrencia.hora, gravidade=ocorrencia.gravidade)
